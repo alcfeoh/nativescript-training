7f88392da3bd073d1694c179d7dd7deb3e11f04c
alcfeoh <alain@interstate21.com>
2020-04-01 16:25:17 -0700

    Added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,38 @@ import {LicensePlate} from "~/app/license-plate";
     template: `
         <StackLayout>
             <ns-hello></ns-hello>
+            <BottomNavigation>
+                <TabStrip>
+                    <TabStripItem class="navigation__item">
+                        <!--
+                            Note TabStripItem will only accept single Label and/or single Image elements that it
+                            will "adopt"; any other layout elements you try to specify will be ignored
+                        -->
+                        <Label text="Home"></Label>
+                        <Image src="font://&#xf015;" class="fas t-36"></Image>
+                    </TabStripItem>
+                    <TabStripItem class="navigation__item">
+                        <Label text="Cart"></Label>
+                        <Image src="font://&#xf07a;" class="fas t-36"></Image>
+                    </TabStripItem>
+                    <TabStripItem class="navigation__item">
+                        <Label text="Checkout"></Label>
+                        <Image src="font://&#xf155;" class="fas t-36"></Image>
+                    </TabStripItem>
+                </TabStrip>
+
+                <TabContentItem>
+                    <page-router-outlet name="homeTab"></page-router-outlet>
+                </TabContentItem>
+
+                <TabContentItem>
+                    <page-router-outlet name="browseTab"></page-router-outlet>
+                </TabContentItem>
+
+                <TabContentItem>
+                    <page-router-outlet name="searchTab"></page-router-outlet>
+                </TabContentItem>
+            </BottomNavigation>
         </StackLayout>
     `
 })
